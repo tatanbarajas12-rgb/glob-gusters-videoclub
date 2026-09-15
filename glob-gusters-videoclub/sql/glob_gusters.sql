19ebe4db28b6a8f9e54eca66a62a5c8b5e6f14b9
Sebastian Joheth Barajas Barba <tatanbarajas12@gmail.com>
2026-09-15 12:12:53 -0500

    glob-gusters-videoclub/sql/glob_gusters.sql

---
+++ glob-gusters-videoclub/sql/glob_gusters.sql
@@ -0,0 +1,11 @@
+-- Base de Datos Relacional Glob-Gusters Video-Club
+-- Autor: Sebastian Joheth Barajas Barba
+-- Fecha: 2026-09-15
+
+
+-- Creación de la base de datos
+CREATE DATABASE IF NOT EXISTS `glob_gusters` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `glob_gusters`;
+
+-- Eliminación de la base de datos
+-- DROP DATABASE IF EXISTS `glob_gusters`;
